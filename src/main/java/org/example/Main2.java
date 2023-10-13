5b1a73fddbd0381980b513df55cabf0439935ff2
VladimirJG <vladimi85@gmail.com>
2023-10-13 11:08:01 +0300

    HW_5_Bean

--- src/main/java/org/example/Main2.java
+++ src/main/java/org/example/Main2.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.dao.MyApplicationContextConfiguration;
 import org.example.dao.UserDao;
 import org.example.model.User;
+import org.example.util.DB;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,5 +16,7 @@ public class Main2 {
         User user2 = dao.findById(1);
         System.out.println(user1);
         System.out.println(user2);
+        DB db = applicationContext.getBean(DB.class);
+        System.out.println(db.getAll());
     }
 }
